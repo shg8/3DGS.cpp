714c4832be1e9cf5570d13950f9d9ed522299993
Steven <38004233+shg8@users.noreply.github.com>
2024-04-01 15:22:27 -0700

    Rename and refactor project (#23)

--- apps/apple/VulkanSplatting/VulkanSplatting-Bridging-Header.h
+++ apps/apple/VulkanSplatting/VulkanSplatting-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#include "VulkanSplatting.h"
+#include "3dgs.h"
 #include <memory>
 #include <iostream>
 
